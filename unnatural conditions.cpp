5c596339177c629cf5148ca61702ed7cda690d18
LordVoldemort <goyal.siddharth06@gmail.com>
2018-08-29 19:22:18 +0530

    This is my first commit

---
+++ unnatural conditions.cpp
@@ -0,0 +1,30 @@
+/*siddharth goyal*/
+#include<bits/stdc++.h>
+using namespace std;
+#define   pb              push_back
+#define   REP(i,n)        for(i=1;i<=n;i++)
+#define   FOR(i,a,b)      for(i=a;i<=b;i++)
+#define   all(v)          v.begin(),v.end()
+#define   F               first
+#define   S               second
+#define   vl              vector<LL>
+#define   itr             ::iterator it
+#define   lb              lower_bound
+#define   ub              upper_bound
+#define   LL              long long
+#define   ULL             unsigned long long
+LL n,i,j;
+int ans ;
+char c[200][200] ;
+int main()
+{   // Read the constraints,highlights,Time Limit,Test Cases
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL) ;
+    LL n,m ;
+    cin>>n>>m ;
+    string s(n,'1') ;
+    string k(n-1,'8');
+    cout<<s<<endl<<k+"9" ;
+
+}
+// Check for 1,0, and other corner cases.
